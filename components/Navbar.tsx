90b337cda66905fc32732a68a5b14c5a743ad1d4
jaykhan-dev <jaykhan.sound@gmail.com>
2023-05-09 13:04:40 -0400

    added web templates db

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,17 +17,17 @@ const links = [
 
 export default function Navbar() {
   return (
-    <nav className="bg-zinc-900 fixed top-0 w-full p-4">
+    <nav className="bg-zinc-900 fixed top-0 w-full">
       <div className="flex items-center justify-between">
         <div>
           <Link href="/">Jay Khan</Link>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="lg:flex items-center hidden">
           {links.map((link: Links) => (
             <Link
               key={link.path}
               href={link.path}
-              className="hover:text-purple-500 duration-300"
+              className="hover:bg-purple-500 duration-300 border-l border-white/50 py-2 px-4"
             >
               {link.label}
             </Link>
--- pages/digital-products/index.tsx
+++ pages/digital-products/index.tsx
@@ -1,13 +1,45 @@
 import React from "react";
+import { notion } from "../../services/notion";
 
-export default function DigitalProducts() {
+interface WebTemplate {
+  id: string;
+  title: string;
+}
+
+export default function DigitalProducts({ webTemplates }: any) {
+  if (!webTemplates) return <div>no data!</div>;
   return (
     <section>
       <div className="h-screen grid place-items-center">
         <div>
-          <h1>Digital Products</h1>
+          <h1 className="lg:text-4xl font-bold">Digital Products</h1>
+          <div>
+            {webTemplates.map((template: any) => (
+              <div key={template.id}>
+                <h2>
+                  {template.properties.Title.title.map((item: any) => (
+                    <span key={item}>{item.plain_text}</span>
+                  ))}
+                </h2>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
   );
+}
+
+export async function getStaticProps() {
+  const webTemplates = process.env.NOTION_WEB_TEMPLATES_DB;
+  const response = await notion.databases.query({
+    database_id: `${webTemplates}`,
+  });
+
+  return {
+    props: {
+      webTemplates: response.results,
+    },
+    revalidate: 1,
+  };
 }
